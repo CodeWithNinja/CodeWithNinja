326aadd012cf2dbf926968821efea3ee4ae9298b
CodeWithNinja <ninja.codewithninja@gmail.com>
2021-10-25 10:04:34 +0530

    Welcome To CodeWithNinja
    Code With Ninja is my attempt to teach basics and those coding techniques to people in short time which took me ages to learn

---
+++ CodeWithNinja.py
@@ -0,0 +1,12 @@
+import pyttsx3
+def speak(text):
+  pyttsx3.speak(text)
+
+sentence1 = "Welcome To CodeWithNinja"
+sentence2 = "Code With Ninja is my attempt to teach basics and those coding techniques to people in short time which took me ages to learn"
+
+print(sentence1)
+speak(sentence1)
+
+print(sentence2)
+speak(sentence2)
